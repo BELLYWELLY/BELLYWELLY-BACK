b9ee30e2f9c7164f7fcf817cb9c97e16002f3592
yeji <yeji.lee.kim@gmail.com>
2024-05-23 11:53:24 +0900

    feat: 특정 날짜의 스트레스 지수를 조회하는 메서드 추가

--- src/main/java/com/capston/bellywelly/domain/record/repository/StressRepository.java
+++ src/main/java/com/capston/bellywelly/domain/record/repository/StressRepository.java
@@ -2,6 +2,7 @@ package com.capston.bellywelly.domain.record.repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -13,4 +14,6 @@ public interface StressRepository extends JpaRepository<Stress, Long> {
 	List<Stress> findAllByMemberAndCreatedDateBetween(Member member, LocalDateTime time1, LocalDateTime time2);
 
 	Boolean existsByMemberAndCreatedDateBetween(Member member, LocalDateTime time1, LocalDateTime time2);
+
+	Optional<Stress> findByMemberAndCreatedDateBetween(Member member, LocalDateTime time1, LocalDateTime time2);
 }
