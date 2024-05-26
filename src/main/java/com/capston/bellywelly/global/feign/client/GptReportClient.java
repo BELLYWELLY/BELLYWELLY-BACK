662acf4914f7c1350fc2300292e93ef5c988d23c
yeji <yeji.lee.kim@gmail.com>
2024-05-26 20:09:04 +0900

    feat: 모델 서버로 레포트 총평 생성 요청을 보내기 위한 feign client와 dto 생성

--- src/main/java/com/capston/bellywelly/global/feign/client/GptReportClient.java
+++ src/main/java/com/capston/bellywelly/global/feign/client/GptReportClient.java
@@ -4,12 +4,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.capston.bellywelly.global.feign.dto.GptDietRequestDto;
+import com.capston.bellywelly.global.feign.dto.GptFeedbackRequestDto;
 import com.capston.bellywelly.global.feign.dto.GptResponseDto;
 
 @FeignClient(name = "chatGptReport", url = "https://model.bellywelly.kro.kr/report")
 public interface GptReportClient {
 
 	@PostMapping
-	GptResponseDto getReportFeedback(@RequestBody GptDietRequestDto requestDto);
+	GptResponseDto getReportFeedback(@RequestBody GptFeedbackRequestDto requestDto);
 }
