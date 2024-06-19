3c49844b537839809a0f75a78fcc68fc4cc8999a
yeji <yeji.lee.kim@gmail.com>
2024-06-19 17:03:24 +0900

    feat: RecommendationController에 스웨거 설명 추가

--- src/main/java/com/capston/bellywelly/domain/recommendation/controller/RecommendationController.java
+++ src/main/java/com/capston/bellywelly/domain/recommendation/controller/RecommendationController.java
@@ -1,14 +1,19 @@
 package com.capston.bellywelly.domain.recommendation.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capston.bellywelly.domain.recommendation.service.RecommendationService;
 import com.capston.bellywelly.global.feign.dto.GptResponseDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "추천", description = "추천 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/recommendations")
@@ -16,7 +21,9 @@ public class RecommendationController {
 
 	private final RecommendationService recommendationService;
 
+	@Operation(summary = "식단 추천")
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public GptResponseDto getDietRecommendation() {
 		return recommendationService.getRecommendation();
 	}
